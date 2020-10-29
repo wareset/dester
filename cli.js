1069def479e8f16024d4fa5a38e5527dc7e78ec2
wareset <git@wareset.com>
2020-10-29 23:13:07 +0500

    dester: v0.0.2

--- cli.js
+++ cli.js
@@ -2,15 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const kleur = require('kleur');
 const argv = require('minimist')(process.argv.slice(2), {
-  default: { external: true, tsc: true },
-  string: ['input', 'output', 'pkg'],
-  boolean: ['external', 'watch', 'tsc'],
+  default: { tsc: true, pkg: true },
+  string: ['input', 'output', 'pkg', 'tsc'],
+  boolean: ['help', 'version', 'watch'],
   alias: {
     h: 'help',
     v: 'version',
     i: 'input',
     o: 'output',
-    e: 'external',
     w: 'watch'
   }
   // '--': true,
@@ -18,8 +17,36 @@ const argv = require('minimist')(process.argv.slice(2), {
   // unknown: function () { ... } /* invoked on unknown param */
 });
 
-(() => {
-  if (argv.version) {
+// console.log(argv);
+
+const rollup = require('rollup');
+
+const rollupPluginCommonjs = require('@rollup/plugin-commonjs')({
+  extensions: ['.mjs', '.cjs', '.js', '.ts', '.tsx']
+});
+const rollupPluginJson = require('@rollup/plugin-json')();
+const rollupPluginSucrase = require('@rollup/plugin-sucrase')({
+  transforms: ['typescript']
+});
+let rollupPluginTypescript;
+
+let input = argv.input || argv._[0] || 'src';
+let output =
+  argv.output ||
+  argv._[1] ||
+  (argv._[0] && input !== argv._[0] ? argv._[0] : 'dist');
+
+input = path.resolve(input);
+output = path.resolve(output);
+
+let pkgFile, tscFile;
+
+let destructor;
+const DESTER = () => {
+  let DESTRUCTOR = () => { };
+  if (destructor) return destructor;
+
+  if (argv.version || argv.help) {
     const red = v => kleur.red(v);
     console.log(
       kleur.cyan().bold(`
@@ -43,29 +70,38 @@ const argv = require('minimist')(process.argv.slice(2), {
     console.log(version);
     console.log(description);
 
+    if (argv.help) {
+      console.log('\nArguments:');
+      console.log('  dester [input] [output]');
+      console.log('  -i, --input  -  Input folder. Default: "src"');
+      console.log('  -o, --output  -  Output folder. Default: "dist"');
+      console.log('  -w, --watch  -  Watch files. Default: false');
+      console.log('  --pkg  -  Path to package.json. Default: "auto"');
+      console.log('  --tsc  -  Path to tsconfig.json. Default: "auto"');
+
+      console.log('\nExamples:');
+      console.log('  dester ./src ./dist');
+      console.log('  dester ./src ./dist --pkg ./pakage.json');
+      console.log('  dester ./src ./dist --no-pkg');
+      console.log('  dester ./src ./dist --watch');
+    }
+
     return;
   }
 
-  // console.log(argv);
+  console.log(kleur.cyan().inverse(' inputDirectory: \n    ' + input + ' '));
+  console.log(kleur.cyan().inverse(' outputDirectory: \n    ' + output + ' '));
+  console.log('');
 
-  let input = argv.input || argv._[0] || 'src';
-  let output =
-    argv.output ||
-    argv._[1] ||
-    (argv._[0] && input !== argv._[0] ? argv._[0] : 'dist');
-
-  input = path.resolve(input);
-  output = path.resolve(output);
-
-  let external = !!argv.external;
-
-  let pkg;
-  if (argv.pkg) {
-    pkg = path.resolve(argv.pkg);
-    if (!path.extname(pkg)) pkg = path.resolve(pkg, 'package.json');
+  let pkg = argv.pkg;
+  try {
+    pkg = JSON.parse(pkg);
+  } catch (err) {
+    /* */
   }
-  if (external && !pkg) {
+  if (pkg === true || pkg === '' || pkg === 'auto') {
     (() => {
+      pkg = '';
       let dir = input;
       let lastDir;
       do {
@@ -78,10 +114,42 @@ const argv = require('minimist')(process.argv.slice(2), {
         dir = path.dirname(dir);
       } while (lastDir !== dir && !pkg);
     })();
-  }
-  if (pkg) pkg = require(pkg);
+  } else if (typeof pkg === 'string') {
+    pkg = path.resolve(argv.pkg);
+    if (!path.extname(pkg)) pkg = path.resolve(pkg, 'package.json');
+  } else pkg = false;
+  if (pkg) pkgFile = require.resolve(pkg), pkg = require(pkg);
 
-  // console.log('pkg', pkg);
+  let tsc = argv.tsc;
+  try {
+    tsc = JSON.parse(tsc);
+  } catch (err) {
+    /* */
+  }
+  if (tsc === true || tsc === '' || tsc === 'auto') {
+    (() => {
+      tsc = '';
+      let dir = input;
+      let lastDir;
+      do {
+        lastDir = dir;
+        try {
+          tsc = require.resolve(path.resolve(dir, 'tsconfig.json'));
+        } catch (err) {
+          /* */
+        }
+        dir = path.dirname(dir);
+      } while (lastDir !== dir && !tsc);
+    })();
+  } else if (typeof tsc === 'string') {
+    tsc = path.resolve(argv.tsc);
+    if (!path.extname(tsc)) tsc = path.resolve(tsc, 'tsconfig.json');
+  } else tsc = false;
+  if (tsc) tscFile = require.resolve(tsc);
+
+  console.log(kleur.cyan().inverse(' pkgFile: \n    ' + pkgFile + ' '));
+  console.log(kleur.cyan().inverse(' tscFile: \n    ' + tscFile + ' '));
+
 
   const BUILD_KEYS = {};
   const findBuilders = (input, output) => {
@@ -134,21 +202,13 @@ const argv = require('minimist')(process.argv.slice(2), {
   findBuilders(input, output);
   // console.log('BUILD_KEYS', BUILD_KEYS);
 
-  external = [].concat(
+  const external = [].concat(
     Object.keys(process.binding('natives')),
     pkg ? Object.keys(pkg.dependencies || {}) : [],
     pkg ? Object.keys(pkg.peerDependencies || {}) : []
   );
 
-  const rollupPluginJson = require('@rollup/plugin-json')();
-  const rollupPluginSucrase = require('@rollup/plugin-sucrase')({
-    transforms: ['typescript']
-  });
-
   // console.log('external', external);
-
-  const rollup = require('rollup');
-  // console.log(rollup);
 
   const startsWith = (s, text, pos = 0) => s.indexOf(text, pos) === pos;
   const resolveExternals = (key, v) => {
@@ -160,15 +220,23 @@ const argv = require('minimist')(process.argv.slice(2), {
   const __types__ = path.resolve(output, '__types__');
 
   if (
-    argv.tsc &&
+    tsc &&
+    !rollupPluginTypescript &&
     Object.keys(BUILD_KEYS).some(v =>
       startsWith(path.parse(BUILD_KEYS[v].input).ext, '.ts')
     )
   ) {
+    if (!rollupPluginTypescript) {
+      rollupPluginTypescript = require('rollup-plugin-typescript2')({
+        tsconfig: tsc,
+        tsconfigOverride: { compilerOptions: { declaration: false } }
+      });
+    }
+
     (() => {
-      let tsc = 'tsc';
+      let cliTS = 'tsc';
       try {
-        tsc = require.resolve('.bin/tsc');
+        cliTS = require.resolve('.bin/tsc');
       } catch (err) {
         /* */
       }
@@ -180,8 +248,9 @@ const argv = require('minimist')(process.argv.slice(2), {
 
       try {
         ts = require('child_process').spawn(
-          tsc,
+          cliTS,
           [
+            ...(tsc ? ['--project', tsc] : []),
             '--rootDir',
             input,
             argv.watch ? '--watch' : '',
@@ -199,6 +268,10 @@ const argv = require('minimist')(process.argv.slice(2), {
 
         process.on('SIGTERM', toExit);
         process.on('exit', toExit);
+        // const DESTRUCTOR_ORIGIN = DESTRUCTOR;
+        // DESTRUCTOR = () => {
+        //   DESTRUCTOR_ORIGIN(), toExit();
+        // };
       } catch (err) {
         /* */
       }
@@ -207,13 +280,12 @@ const argv = require('minimist')(process.argv.slice(2), {
 
   (() => {
     const ROLLS = [];
-    let lastRollInput;
     for (const key of Object.keys(BUILD_KEYS)) {
       const data = BUILD_KEYS[key];
-      lastRollInput = data.input;
-      const isTS = path.parse(data.input).ext === '.ts';
-      const plugins = [rollupPluginJson];
-      if (isTS) plugins.push(rollupPluginSucrase);
+      const isTS = startsWith(path.parse(data.input).ext, '.ts');
+      const plugins = [];
+      if (rollupPluginTypescript) plugins.push(rollupPluginTypescript);
+      plugins.push(rollupPluginSucrase, rollupPluginCommonjs, rollupPluginJson);
 
       const inputOptions = {
         external: [
@@ -237,7 +309,7 @@ const argv = require('minimist')(process.argv.slice(2), {
           (__package__.main = './index'), (__package__.module = './index.mjs');
           delete __package__.types;
 
-          if (isTS && argv.tsc) {
+          if (isTS && tsc) {
             __package__.types = './index.d.ts';
 
             const importPath = resolveExternals(
@@ -268,22 +340,58 @@ const argv = require('minimist')(process.argv.slice(2), {
     process.on('SIGTERM', watcher.close);
     process.on('exit', watcher.close);
 
+    const DESTRUCTOR_ORIGIN = DESTRUCTOR;
+    DESTRUCTOR = () => {
+      DESTRUCTOR_ORIGIN(), watcher.close();
+    };
+
     watcher.on('event', event => {
+      // console.log(event, watcher);
       // event.code can be one of:
       //   START        — the watcher is (re)starting
       //   BUNDLE_START — building an individual bundle
       //   BUNDLE_END   — finished building a bundle
       //   END          — finished building all bundles
       //   ERROR        — encountered an error while bundling
+
       if (event.code === 'BUNDLE_START') {
-        console.log(kleur.cyan(event.input));
-        return;
+        console.log('\n');
+        console.log(kleur.inverse(' ' + event.input + ' '));
       }
 
       if (event.code === 'BUNDLE_END') {
         console.log('--------------------------------------------------------');
-        if (!argv.watch && event.input === lastRollInput) watcher.close();
+      }
+
+      if (event.code === 'END' && !argv.watch) {
+        watcher.close();
+      }
+
+      if (event.code === 'ERROR') {
+        console.error(event);
+        if (!argv.watch) {
+          watcher.close();
+          process.kill(0);
+          process.exit(0);
+        }
       }
     });
   })();
-})();
+
+  return DESTRUCTOR;
+};
+
+destructor = DESTER();
+const redester = force => (type, file) => {
+  console.log('\n');
+  console.log(kleur.red().inverse(' ' + type + ': ' + file + ' '));
+  if (type === 'rename' || force) {
+    if (destructor) destructor(), destructor = null;
+    setTimeout(() => (destructor = DESTER()), 1000);
+  }
+};
+if (argv.watch) {
+  fs.watch(input, redester());
+  if (pkgFile) fs.watch(pkgFile, redester(true));
+  if (tscFile) fs.watch(tscFile, redester(true));
+}
