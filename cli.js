0a2a04f54ff1c17b0810b0352edee9e0aa738413
wareset <git@wareset.com>
2020-10-30 04:10:36 +0500

    dester: v0.0.3

--- cli.js
+++ cli.js
@@ -1,15 +1,29 @@
 const fs = require('fs');
 const path = require('path');
+
+// prettier-ignore
+const hash = s => (s.replace(/\r/g, '').split('').reduce((a, b) =>
+  (a = (a << 5) - a + b.charCodeAt(0)), 0) >>> 0).toString(36);
+const startsWith = (s, text, pos = 0) => s.indexOf(text, pos) === pos;
+const resolveExternals = (key, v) => {
+  let res = v !== undefined ? path.relative(key, v) : key;
+  if (!startsWith(res, '/') && !startsWith(res, '..')) res = './' + res;
+  return res;
+};
+
+const parseJSON = str => JSON.parse(str.replace(/^\s*\/\/.*/gm, ''));
+
 const kleur = require('kleur');
 const argv = require('minimist')(process.argv.slice(2), {
-  default: { tsc: true, pkg: true },
+  default: { tsc: true, pkg: true, types: true },
   string: ['input', 'output', 'pkg', 'tsc'],
-  boolean: ['help', 'version', 'watch'],
+  boolean: ['help', 'version', 'watch', 'types'],
   alias: {
     h: 'help',
     v: 'version',
     i: 'input',
     o: 'output',
+    t: 'types',
     w: 'watch'
   }
   // '--': true,
@@ -19,16 +33,7 @@ const argv = require('minimist')(process.argv.slice(2), {
 
 // console.log(argv);
 
-const rollup = require('rollup');
-
-const rollupPluginCommonjs = require('@rollup/plugin-commonjs')({
-  extensions: ['.mjs', '.cjs', '.js', '.ts', '.tsx']
-});
-const rollupPluginJson = require('@rollup/plugin-json')();
-const rollupPluginSucrase = require('@rollup/plugin-sucrase')({
-  transforms: ['typescript']
-});
-let rollupPluginTypescript;
+const EXTENSIONS = ['.mjs', '.cjs', '.js', '.jsx', '.ts', '.tsx'];
 
 let input = argv.input || argv._[0] || 'src';
 let output =
@@ -36,16 +41,62 @@ let output =
   argv._[1] ||
   (argv._[0] && input !== argv._[0] ? argv._[0] : 'dist');
 
-input = path.resolve(input);
-output = path.resolve(output);
+const findExternalsFn = (input, output) => {
+  const BUILD_KEYS = {};
 
-let pkgFile, tscFile;
+  const createOutputFn = (output, name = '') => [
+    { file: path.resolve(output, name, 'index.mjs'), format: 'esm' },
+    { file: path.resolve(output, name, 'index.js'), format: 'cjs' }
+  ];
 
-let destructor;
-const DESTER = () => {
-  let DESTRUCTOR = () => { };
-  if (destructor) return destructor;
+  const findBuilders = (input, output) => {
+    const files = fs.readdirSync(input, { withFileTypes: true });
 
+    let name, ext, key;
+    const isIndex = [...files].some(dirent => {
+      ({ name, ext } = path.parse(dirent.name));
+      if (dirent.isFile() && name === 'index' && ~EXTENSIONS.indexOf(ext)) {
+        BUILD_KEYS[output] = {
+          name: dirent.name,
+          input: path.resolve(input, dirent.name),
+          output: createOutputFn(output)
+        };
+        return true;
+      }
+    });
+
+    for (const dirent of files) {
+      name = dirent.name;
+      if (name[0] === '_') continue;
+      if (dirent.isDirectory()) {
+        findBuilders(path.resolve(input, name), path.resolve(output, name));
+      } else if (!isIndex && dirent.isFile()) {
+        ({ name, ext } = path.parse(name)), (key = path.resolve(output, name));
+        if (!(key in BUILD_KEYS) && ~EXTENSIONS.indexOf(ext)) {
+          BUILD_KEYS[key] = {
+            name: dirent.name,
+            input: path.resolve(input, dirent.name),
+            output: createOutputFn(output, name)
+          };
+        }
+      }
+    }
+  };
+
+  findBuilders(input, output);
+  return BUILD_KEYS;
+};
+
+const rollup = require('rollup');
+const rollupPluginCommonjs = require('@rollup/plugin-commonjs')({
+  extensions: EXTENSIONS
+});
+const rollupPluginJson = require('@rollup/plugin-json')();
+const rollupPluginSucrase = require('@rollup/plugin-sucrase')({
+  transforms: ['typescript']
+});
+
+(() => {
   if (argv.version || argv.help) {
     const red = v => kleur.red(v);
     console.log(
@@ -57,14 +108,6 @@ const DESTER = () => {
     `)
     );
 
-    //     console.log(
-    //       `
-    //     ___       __ _ _ _ _ /_,_
-    //    / _ \\_(/(/(_(/ (-_)(-/_ _)
-    //   / _/ / -_/_ —/ __/ -_/ __/
-    //  /____/\\__/___/\\__/\\__/_/
-    // `
-    //     );
     const { name, version, description } = require('./package.json');
     console.log(name);
     console.log(version);
@@ -89,214 +132,143 @@ const DESTER = () => {
     return;
   }
 
-  console.log(kleur.cyan().inverse(' inputDirectory: \n    ' + input + ' '));
-  console.log(kleur.cyan().inverse(' outputDirectory: \n    ' + output + ' '));
-  console.log('');
+  const DIR_INPUT = path.resolve(input);
+  const DIR_OUTPUT = path.resolve(output);
+  const DIR_TYPES = path.resolve(DIR_OUTPUT, '__types__');
 
-  let pkg = argv.pkg;
-  try {
-    pkg = JSON.parse(pkg);
-  } catch (err) {
-    /* */
-  }
-  if (pkg === true || pkg === '' || pkg === 'auto') {
-    (() => {
-      pkg = '';
-      let dir = input;
-      let lastDir;
-      do {
-        lastDir = dir;
-        try {
-          pkg = require.resolve(path.resolve(dir, 'package.json'));
-        } catch (err) {
-          /* */
-        }
-        dir = path.dirname(dir);
-      } while (lastDir !== dir && !pkg);
-    })();
-  } else if (typeof pkg === 'string') {
-    pkg = path.resolve(argv.pkg);
-    if (!path.extname(pkg)) pkg = path.resolve(pkg, 'package.json');
-  } else pkg = false;
-  if (pkg) pkgFile = require.resolve(pkg), pkg = require(pkg);
+  // console.log(process);
+  const DIR_NODE_MODULES = require.main.paths[0];
+  const DIR_CACHE = path.resolve(DIR_NODE_MODULES, '.cache');
+  if (!fs.existsSync(DIR_CACHE)) fs.mkdirSync(DIR_CACHE);
+  const DIR_CACHE_DESTER = path.resolve(DIR_CACHE, 'dester');
+  if (!fs.existsSync(DIR_CACHE_DESTER)) fs.mkdirSync(DIR_CACHE_DESTER);
+  const FILE_TSCONFIG = path.resolve(
+    DIR_CACHE_DESTER,
+    hash(DIR_INPUT + DIR_OUTPUT) + '.json'
+  );
 
-  let tsc = argv.tsc;
-  try {
-    tsc = JSON.parse(tsc);
-  } catch (err) {
-    /* */
-  }
-  if (tsc === true || tsc === '' || tsc === 'auto') {
-    (() => {
-      tsc = '';
-      let dir = input;
-      let lastDir;
-      do {
-        lastDir = dir;
-        try {
-          tsc = require.resolve(path.resolve(dir, 'tsconfig.json'));
-        } catch (err) {
-          /* */
-        }
-        dir = path.dirname(dir);
-      } while (lastDir !== dir && !tsc);
-    })();
-  } else if (typeof tsc === 'string') {
-    tsc = path.resolve(argv.tsc);
-    if (!path.extname(tsc)) tsc = path.resolve(tsc, 'tsconfig.json');
-  } else tsc = false;
-  if (tsc) tscFile = require.resolve(tsc);
+  const OBJ_TSCONFIG = {
+    // Change this to match your project
+    include: [path.join(DIR_INPUT, '/**/*')],
 
-  console.log(kleur.cyan().inverse(' pkgFile: \n    ' + pkgFile + ' '));
-  console.log(kleur.cyan().inverse(' tscFile: \n    ' + tscFile + ' '));
-
-
-  const BUILD_KEYS = {};
-  const findBuilders = (input, output) => {
-    const files = fs.readdirSync(input, { withFileTypes: true });
-    // console.log(files);
-
-    const isIndex = [...files].some(dirent => {
-      if (
-        dirent.isFile() &&
-        path.parse(dirent.name).name === 'index' &&
-        !(output in BUILD_KEYS)
-      ) {
-        BUILD_KEYS[output] = {
-          name: dirent.name,
-          input: path.resolve(input, dirent.name),
-          output: [
-            { file: path.resolve(output, 'index.mjs'), format: 'esm' },
-            { file: path.resolve(output, 'index.js'), format: 'cjs' }
-          ]
-        };
-        return true;
-      }
-    });
-
-    let name, key;
-    for (const dirent of files) {
-      name = dirent.name;
-      if (name[0] === '_') continue;
-      if (dirent.isDirectory()) {
-        findBuilders(path.resolve(input, name), path.resolve(output, name));
-      } else if (!isIndex && dirent.isFile()) {
-        name = path.parse(name).name;
-        key = path.resolve(output, name);
-        // continue;
-        if (!(key in BUILD_KEYS)) {
-          BUILD_KEYS[key] = {
-            name: dirent.name,
-            input: path.resolve(input, dirent.name),
-            output: [
-              { file: path.resolve(output, name, 'index.mjs'), format: 'esm' },
-              { file: path.resolve(output, name, 'index.js'), format: 'cjs' }
-            ]
-          };
-        }
-      }
-      // console.log(dirent);
+    compilerOptions: {
+      // Tells TypeScript to read JS files, as
+      // normally they are ignored as source files
+      allowJs: true,
+      // Generate d.ts files
+      declaration: true,
+      // This compiler run should
+      // only output d.ts files
+      emitDeclarationOnly: true,
+      // Types should go into this directory.
+      // Removing this would place the .d.ts files
+      // next to the .js files
+      outDir: DIR_TYPES
     }
   };
 
-  findBuilders(input, output);
-  // console.log('BUILD_KEYS', BUILD_KEYS);
+  fs.writeFileSync(FILE_TSCONFIG, JSON.stringify(OBJ_TSCONFIG));
+  process.on('exit', () => fs.unlinkSync(FILE_TSCONFIG));
 
-  const external = [].concat(
-    Object.keys(process.binding('natives')),
-    pkg ? Object.keys(pkg.dependencies || {}) : [],
-    pkg ? Object.keys(pkg.peerDependencies || {}) : []
-  );
+  const createTypesFn = () => {
+    let cliTS = 'tsc';
 
-  // console.log('external', external);
+    // prettier-ignore
+    // eslint-disable-next-line block-spacing, brace-style
+    try { cliTS = require.resolve('.bin/tsc'); } catch (err) { /* */ }
 
-  const startsWith = (s, text, pos = 0) => s.indexOf(text, pos) === pos;
-  const resolveExternals = (key, v) => {
-    let res = v !== undefined ? path.relative(key, v) : key;
-    if (!startsWith(res, '/') && !startsWith(res, '..')) res = './' + res;
-    return res;
+    // prettier-ignore
+    try {
+      const tsFork = require('child_process').spawn(cliTS,
+        ['--build', FILE_TSCONFIG, ...(argv.watch ? ['--watch'] : [])]
+        // { stdio: ['ignore', 'inherit', 'inherit'], shell: true }
+      );
+      const exit = () => tsFork && tsFork.kill(0);
+      process.on('SIGTERM', exit), process.on('exit', exit);
+      tsFork.stdout.on('data', data => {
+        console.log([data.toString()]);
+      });
+    } catch (err) {/* */}
+  };
+  if (argv.tsc !== false && argv.types) createTypesFn();
+
+  input = path.resolve(input);
+  output = path.resolve(output);
+
+  const findFileFn = (fileFromArgv, base) => {
+    let file = fileFromArgv;
+
+    // prettier-ignore
+    // eslint-disable-next-line block-spacing, brace-style
+    try { file = JSON.parse(file); } catch (err) { /* */ }
+
+    if (file === true || file === '' || file === 'auto') {
+      (() => {
+        file = '';
+        let dir = DIR_INPUT;
+        let lastDir;
+        do {
+          lastDir = dir;
+          // prettier-ignore
+          // eslint-disable-next-line block-spacing, brace-style
+          try { file = require.resolve(path.resolve(dir, base)); }
+          catch (err) {/* */}
+          dir = path.dirname(dir);
+        } while (lastDir !== dir && !file);
+      })();
+    } else if (typeof file === 'string') {
+      file = path.resolve(fileFromArgv);
+      if (!path.extname(file)) file = path.resolve(file, base);
+    } else file = false;
+    return file;
   };
 
-  const __types__ = path.resolve(output, '__types__');
+  const pkgFile = findFileFn(argv.pkg, 'package.json');
+  const tscFile = findFileFn(argv.tsc, 'tsconfig.json');
 
-  if (
-    tsc &&
-    !rollupPluginTypescript &&
-    Object.keys(BUILD_KEYS).some(v =>
-      startsWith(path.parse(BUILD_KEYS[v].input).ext, '.ts')
-    )
-  ) {
-    if (!rollupPluginTypescript) {
+  console.log(kleur.cyan().inverse(' DIR_INPUT: \n    ' + DIR_INPUT + ' '));
+  console.log(kleur.cyan().inverse(' DIR_OUTPUT: \n    ' + DIR_OUTPUT + ' '));
+  console.log('');
+  console.log(kleur.cyan().inverse(' FILE_PKG: \n    ' + pkgFile + ' '));
+  console.log(kleur.cyan().inverse(' FILE_TSC: \n    ' + tscFile + ' '));
+
+  let watcher;
+  const buildRollupFn = () => {
+    // if (watcher) watcher.close(), watcher = null;
+    if (watcher) return watcher;
+
+    const BUILD_KEYS = findExternalsFn(DIR_INPUT, DIR_OUTPUT);
+    // console.log(BUILD_KEYS);
+
+    let pkg;
+    if (pkgFile) pkg = parseJSON(fs.readFileSync(pkgFile).toString());
+
+    let rollupPluginTypescript;
+    if (
+      tscFile &&
+      Object.keys(BUILD_KEYS).some(v =>
+        startsWith(path.parse(BUILD_KEYS[v].input).ext, '.ts')
+      )
+    ) {
       rollupPluginTypescript = require('rollup-plugin-typescript2')({
-        tsconfig: tsc,
+        tsconfig: tscFile,
         tsconfigOverride: { compilerOptions: { declaration: false } }
       });
     }
 
-    (() => {
-      let cliTS = 'tsc';
-      try {
-        cliTS = require.resolve('.bin/tsc');
-      } catch (err) {
-        /* */
-      }
+    const external = [].concat(
+      Object.keys(process.binding('natives')),
+      pkg ? Object.keys(pkg.dependencies || {}) : [],
+      pkg ? Object.keys(pkg.peerDependencies || {}) : []
+    );
 
-      let ts;
-      function toExit() {
-        if (ts) ts.kill(0);
-      }
-
-      try {
-        ts = require('child_process').spawn(
-          cliTS,
-          [
-            ...(tsc ? ['--project', tsc] : []),
-            '--rootDir',
-            input,
-            argv.watch ? '--watch' : '',
-            '--emitDeclarationOnly',
-            '--declarationDir',
-            __types__,
-            '--resolveJsonModule',
-            '--declaration'
-          ],
-          {
-            stdio: ['ignore', 'inherit', 'inherit'],
-            shell: true
-          }
-        );
-
-        process.on('SIGTERM', toExit);
-        process.on('exit', toExit);
-        // const DESTRUCTOR_ORIGIN = DESTRUCTOR;
-        // DESTRUCTOR = () => {
-        //   DESTRUCTOR_ORIGIN(), toExit();
-        // };
-      } catch (err) {
-        /* */
-      }
-    })();
-  }
-
-  (() => {
     const ROLLS = [];
     for (const key of Object.keys(BUILD_KEYS)) {
-      const data = BUILD_KEYS[key];
-      const isTS = startsWith(path.parse(data.input).ext, '.ts');
+      const { input, output } = BUILD_KEYS[key];
       const plugins = [];
-      if (rollupPluginTypescript) plugins.push(rollupPluginTypescript);
+      const isTs = startsWith(path.parse(input).ext, '.ts');
+      if (isTs && rollupPluginTypescript) plugins.push(rollupPluginTypescript);
       plugins.push(rollupPluginSucrase, rollupPluginCommonjs, rollupPluginJson);
-
-      const inputOptions = {
-        external: [
-          ...external,
-          ...Object.keys(BUILD_KEYS)
-            .filter(v => v !== key)
-            .map(v => resolveExternals(key, v))
-        ],
-        input: data.input,
-        plugins
-      };
 
       const filePackage = path.resolve(key, 'package.json');
 
@@ -304,18 +276,16 @@ const DESTER = () => {
         writeBundle() {
           let __package__ = {};
           if (fs.existsSync(filePackage)) {
-            __package__ = JSON.parse(fs.readFileSync(filePackage).toString());
+            __package__ = parseJSON(fs.readFileSync(filePackage).toString());
           }
           (__package__.main = './index'), (__package__.module = './index.mjs');
-          delete __package__.types;
 
-          if (isTS && tsc) {
+          if (argv.tsc !== false && argv.types) {
             __package__.types = './index.d.ts';
-
             const importPath = resolveExternals(
               path.join(
-                resolveExternals(key, __types__),
-                resolveExternals(output, key)
+                resolveExternals(key, DIR_TYPES),
+                resolveExternals(DIR_OUTPUT, key)
               )
             );
 
@@ -323,7 +293,7 @@ const DESTER = () => {
               path.resolve(key, 'index.d.ts'),
               `export * from '${importPath}';`
             );
-          }
+          } else delete __package__.types;
 
           fs.writeFileSync(
             filePackage,
@@ -332,18 +302,23 @@ const DESTER = () => {
         }
       });
 
-      const watchOptions = { ...inputOptions, output: data.output };
+      const watchOptions = {
+        external: [
+          ...external,
+          ...Object.keys(BUILD_KEYS)
+            .filter(v => v !== key)
+            .map(v => resolveExternals(key, v))
+        ],
+        input,
+        plugins,
+        output
+      };
+
       ROLLS.push(watchOptions);
     }
 
-    const watcher = rollup.watch(ROLLS);
-    process.on('SIGTERM', watcher.close);
-    process.on('exit', watcher.close);
-
-    const DESTRUCTOR_ORIGIN = DESTRUCTOR;
-    DESTRUCTOR = () => {
-      DESTRUCTOR_ORIGIN(), watcher.close();
-    };
+    watcher = rollup.watch(ROLLS);
+    process.on('SIGTERM', watcher.close), process.on('exit', watcher.close);
 
     watcher.on('event', event => {
       // console.log(event, watcher);
@@ -363,35 +338,37 @@ const DESTER = () => {
         console.log('--------------------------------------------------------');
       }
 
-      if (event.code === 'END' && !argv.watch) {
-        watcher.close();
-      }
+      if (event.code === 'END' && !argv.watch) watcher.close();
 
       if (event.code === 'ERROR') {
         console.error(event);
-        if (!argv.watch) {
-          watcher.close();
-          process.kill(0);
-          process.exit(0);
-        }
+        if (!argv.watch) watcher.close(), process.kill(0), process.exit(0);
       }
     });
-  })();
 
-  return DESTRUCTOR;
-};
+    return watcher;
+  };
 
-destructor = DESTER();
-const redester = force => (type, file) => {
-  console.log('\n');
-  console.log(kleur.red().inverse(' ' + type + ': ' + file + ' '));
-  if (type === 'rename' || force) {
-    if (destructor) destructor(), destructor = null;
-    setTimeout(() => (destructor = DESTER()), 1000);
+  watcher = buildRollupFn();
+  let last;
+  const rebuildFn = force => (type, file) => {
+    const l = type + file;
+
+    if (last !== l) {
+      last = l;
+      console.log('\n');
+      console.log(kleur.red().inverse(' ' + type + ': ' + file + ' '));
+    }
+
+    if (type !== 'change' || force) {
+      if (watcher) watcher.close(), (watcher = null);
+      setTimeout(() => (watcher = buildRollupFn()), 1000);
+    }
+  };
+  if (argv.watch) {
+    const chokidar = require('chokidar');
+    chokidar.watch(input).on('raw', rebuildFn());
+    if (pkgFile) chokidar.watch(pkgFile).on('change', rebuildFn(true));
+    if (tscFile) chokidar.watch(tscFile).on('change', rebuildFn(true));
   }
-};
-if (argv.watch) {
-  fs.watch(input, redester());
-  if (pkgFile) fs.watch(pkgFile, redester(true));
-  if (tscFile) fs.watch(tscFile, redester(true));
-}
+})();
